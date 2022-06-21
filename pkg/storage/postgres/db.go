14626e292e9fee48d1b55a1b28cb20231af010a1
Ebin Elbentor <ebinelbentor@Ebins-MacBook-Pro.local>
2022-06-21 18:47:37 +0700

    update db.go in postgres storage

--- pkg/storage/postgres/db.go
+++ pkg/storage/postgres/db.go
@@ -1,1 +1,28 @@
 package postgres
+
+import (
+	"context"
+	"database/sql"
+)
+
+type Queries interface {
+	ExecContext(context.Context, string, ...interface{}) (sql.Result, error)
+	PrepareContext(context.Context, string) (*sql.Stmt, error)
+	QueryContext(context.Context, string, ...interface{}) (*sql.Rows, error)
+	QueryRowContext(context.Context, string, ...interface{}) *sql.Row
+}
+
+type Transaction interface {
+	Rollback() error
+	Commit() error
+}
+
+type TrxOpener interface {
+	BeginTx(context.Context, *sql.TxOptions) (*sql.Tx, error)
+}
+
+type PostgreSQL interface {
+	Queries
+	Transaction
+	TrxOpener
+}
