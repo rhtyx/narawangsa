0b8c9befffca4f9f939bf57cb5563ed7f79e1d8a
Ebin Elbentor <ebinelbentor@Ebins-MacBook-Pro.local>
2022-06-24 12:53:10 +0700

    change cost to bcryptDefault

--- lib/password.go
+++ lib/password.go
@@ -7,7 +7,7 @@ import (
 )
 
 func HashPassword(password string) (string, error) {
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.MaxCost)
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
 		return "", fmt.Errorf("failed to hash password: %v", err)
 	}
