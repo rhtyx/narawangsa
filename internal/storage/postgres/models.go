96684c9615ab5cb2f674f495127ffd65163040ec
Ebin Elbentor <ebinelbentor@Ebins-MacBook-Pro.local>
2022-06-24 16:39:53 +0700

    add Category struct to models.go

--- internal/storage/postgres/models.go
+++ internal/storage/postgres/models.go
@@ -33,3 +33,10 @@ type BookList struct {
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
+
+type Category struct {
+	ID        int64     `json:"id"`
+	Name      string    `json:"name"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
