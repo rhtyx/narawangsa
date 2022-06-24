87e857fb86fdfadb4067f6eca3d3be454a8c2fd5
Ebin Elbentor <ebinelbentor@Ebins-MacBook-Pro.local>
2022-06-24 12:52:44 +0700

    update password_test.go

--- go.mod
+++ go.mod
@@ -2,4 +2,13 @@ module github.com/rhtyx/narawangsa
 
 go 1.17
 
-require golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d
+require (
+	github.com/stretchr/testify v1.7.5
+	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- lib/password_test.go
+++ lib/password_test.go
@@ -1,1 +1,23 @@
 package lib
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func TestPassword(t *testing.T) {
+	password := "abcdef"
+	wrongPassword := "gfedcb"
+
+	hashedPassword, err := HashPassword(password)
+	require.NoError(t, err)
+	require.NotEmpty(t, hashedPassword)
+
+	err = CheckPassword(password, hashedPassword)
+	require.NoError(t, err)
+
+	err = CheckPassword(wrongPassword, hashedPassword)
+	require.EqualError(t, err, bcrypt.ErrMismatchedHashAndPassword.Error())
+}
