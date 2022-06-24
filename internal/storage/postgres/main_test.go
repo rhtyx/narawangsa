ec3ecbf9de0825063803d067f114bc094ac855a9
Ebin Elbentor <ebinelbentor@Ebins-MacBook-Pro.local>
2022-06-24 15:10:34 +0700

    update main_test.go - add connection to postgres implemented to all tests in package postgres

--- internal/storage/postgres/main_test.go
+++ internal/storage/postgres/main_test.go
@@ -1,1 +1,28 @@
 package postgres
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://narawangsa:narawangsa@localhost:5434/narawangsa_db?sslmode=disable"
+)
+
+var testQueries *Queries
+
+func TestMain(m *testing.M) {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db", err)
+	}
+
+	testQueries = NewQueries(conn)
+
+	os.Exit(m.Run())
+}
