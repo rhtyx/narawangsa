ddc8220db43111d49f42b3ff9ced875d67c7f5aa
Ebin Elbentor <ebinelbentor@Ebins-MacBook-Pro.local>
2022-06-24 16:31:51 +0700

    update models.go - added struct Book

--- internal/storage/postgres/models.go
+++ internal/storage/postgres/models.go
@@ -11,3 +11,14 @@ type User struct {
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
+
+type Book struct {
+	ID        int64     `json:"id"`
+	Title     string    `json:"title"`
+	Author    string    `json:"author"`
+	Year      string    `json:"year"`
+	Pages     int32     `json:"pages"`
+	Synopsis  string    `json:"synopsis"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
