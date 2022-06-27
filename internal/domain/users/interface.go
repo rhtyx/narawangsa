8de3b499235103c6130a6c26c29acba11f9d5640
Ebin Elbentor <ebinelbentor@Ebins-MacBook-Pro.local>
2022-06-27 15:51:33 +0700

    update interface.go in package users - added IUsers interface

--- internal/domain/users/interface.go
+++ internal/domain/users/interface.go
@@ -1,1 +1,16 @@
 package users
+
+import (
+	"context"
+
+	"github.com/rhtyx/narawangsa/internal/storage/postgres"
+)
+
+type IUsers interface {
+	CreateUser(ctx context.Context, arg postgres.CreateUserParams) (int64, error)
+	DeleteUser(ctx context.Context, username string) error
+	GetUser(ctx context.Context, username string) (postgres.User, error)
+	UpdatePasswordUser(ctx context.Context, arg postgres.UpdatePasswordUserParams) error
+	UpdateUser(ctx context.Context, arg postgres.UpdateUserParams) (postgres.UpdateUserRow, error)
+	CreateUserLevel(ctx context.Context, userID int64) error
+}
