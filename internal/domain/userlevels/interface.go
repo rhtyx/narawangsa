84dd45f1173c114e389a57174dc6ee1eeaadc6ac
Ebin Elbentor <ebinelbentor@Ebins-MacBook-Pro.local>
2022-06-27 21:14:01 +0700

    update interface.go in package userlevels - added interface IUserLvels

--- internal/domain/userlevels/interface.go
+++ internal/domain/userlevels/interface.go
@@ -1,1 +1,12 @@
 package userlevels
+
+import (
+	"context"
+
+	"github.com/rhtyx/narawangsa/internal/storage/postgres"
+)
+
+type IUserLevels interface {
+	GetUserLevel(ctx context.Context, userID int64) (postgres.GetUserLevelRow, error)
+	UpdateUserLevelsLevel(ctx context.Context, arg postgres.UpdateUserLevelsLevelParams) error
+}
