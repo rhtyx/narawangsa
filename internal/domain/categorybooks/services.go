cdaa2031ff55f0abd363b012c3bf3662e1078bd1
Ebin Elbentor <ebinelbentor@Ebins-MacBook-Pro.local>
2022-06-28 17:19:14 +0700

    update services.go in package categorybooks - added NewCategoryBooksService functionality

--- internal/domain/categorybooks/services.go
+++ internal/domain/categorybooks/services.go
@@ -1,1 +1,37 @@
 package categorybooks
+
+import (
+	"context"
+
+	"github.com/rhtyx/narawangsa/internal/storage/postgres"
+)
+
+type categoryBooksStorage interface {
+	CreateBookCategory(ctx context.Context, arg postgres.CreateBookCategoryParams) error
+	DeleteBookCategory(ctx context.Context, arg postgres.DeleteBookCategoryParams) error
+}
+
+type service struct {
+	repository ICategoryBooks
+	tx         postgres.TxInContext
+}
+
+func NewCategoryBooksService(repository ICategoryBooks, tx postgres.TxInContext) categoryBooksStorage {
+	return &service{
+		repository: repository,
+		tx:         tx,
+	}
+}
+
+func (s *service) CreateBookCategory(ctx context.Context, arg postgres.CreateBookCategoryParams) error {
+	err := s.tx.Run(ctx, func(ctx context.Context) error {
+		return s.repository.CreateBookCategory(ctx, arg)
+	})
+	return err
+}
+func (s *service) DeleteBookCategory(ctx context.Context, arg postgres.DeleteBookCategoryParams) error {
+	err := s.tx.Run(ctx, func(ctx context.Context) error {
+		return s.repository.DeleteBookCategory(ctx, arg)
+	})
+	return err
+}
