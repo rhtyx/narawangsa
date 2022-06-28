9d2eb51e64d7956f1e9977fe73c5f0be520ff69d
Ebin Elbentor <ebinelbentor@Ebins-MacBook-Pro.local>
2022-06-28 17:13:52 +0700

    update interface.go in package categorybooks - added interface ICategoryBooks

--- internal/domain/categorybooks/interface.go
+++ internal/domain/categorybooks/interface.go
@@ -1,1 +1,12 @@
 package categorybooks
+
+import (
+	"context"
+
+	"github.com/rhtyx/narawangsa/internal/storage/postgres"
+)
+
+type ICategoryBooks interface {
+	CreateBookCategory(ctx context.Context, arg postgres.CreateBookCategoryParams) error
+	DeleteBookCategory(ctx context.Context, arg postgres.DeleteBookCategoryParams) error
+}
