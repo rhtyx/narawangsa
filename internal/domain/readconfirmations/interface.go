a634b32569b3d2090741c9c56f0305bcfa84be6d
Ebin Elbentor <ebinelbentor@Ebins-MacBook-Pro.local>
2022-06-28 23:23:51 +0700

    update interface.go in package readconfirmations - added interface IReadConfirmations

--- internal/domain/readconfirmations/interface.go
+++ internal/domain/readconfirmations/interface.go
@@ -1,1 +1,12 @@
 package readconfirmations
+
+import (
+	"context"
+
+	"github.com/rhtyx/narawangsa/internal/storage/postgres"
+)
+
+type IReadConfirmations interface {
+	CreateReadConfirmation(ctx context.Context, arg postgres.CreateReadConfirmationParams) error
+	ListReadConfirmations(ctx context.Context, arg postgres.ListReadConfirmationsParams) ([]postgres.ListReadConfirmationsRow, error)
+}
