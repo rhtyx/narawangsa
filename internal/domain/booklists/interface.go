d1a088afe51715e3f7080b609cdcec20f7fd8ef3
Ebin Elbentor <ebinelbentor@Ebins-MacBook-Pro.local>
2022-06-28 18:20:46 +0700

    update interface.go in package booklists - added interface IBooklists

--- internal/domain/booklists/interface.go
+++ internal/domain/booklists/interface.go
@@ -1,1 +1,14 @@
 package booklists
+
+import (
+	"context"
+
+	"github.com/rhtyx/narawangsa/internal/storage/postgres"
+)
+
+type IBooklists interface {
+	CreateBookList(ctx context.Context, arg postgres.CreateBookListParams) error
+	DeleteBookList(ctx context.Context, bookID int64) error
+	ListBookList(ctx context.Context, userID int64) ([]postgres.BookList, error)
+	UpdateBookList(ctx context.Context, arg postgres.UpdateBookListParams) error
+}
