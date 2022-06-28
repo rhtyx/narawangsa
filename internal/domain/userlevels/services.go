5abf13d97d6424ad0b61deac52116f97872dc8f3
Ebin Elbentor <ebinelbentor@Ebins-MacBook-Pro.local>
2022-06-28 08:32:45 +0700

    update services.go in package userlevels - added function NewUserLevelsService

--- internal/domain/userlevels/services.go
+++ internal/domain/userlevels/services.go
@@ -1,1 +1,48 @@
 package userlevels
+
+import (
+	"context"
+
+	"github.com/rhtyx/narawangsa/internal/storage/postgres"
+)
+
+type userLevelsStorage interface {
+	GetUserLevel(ctx context.Context, userID int64) (postgres.GetUserLevelRow, error)
+	UpdateUserLevelsLevel(ctx context.Context, arg postgres.UpdateUserLevelsLevelParams) error
+}
+
+type service struct {
+	repository IUserLevels
+	tx         postgres.TxInContext
+}
+
+func NewUserLevelsService(repository IUserLevels, tx postgres.TxInContext) userLevelsStorage {
+	return &service{
+		repository: repository,
+		tx:         tx,
+	}
+}
+
+func (s *service) GetUserLevel(ctx context.Context, userID int64) (postgres.GetUserLevelRow, error) {
+	var userLevel postgres.GetUserLevelRow
+	err := s.tx.Run(ctx, func(ctx context.Context) error {
+		ul, err := s.repository.GetUserLevel(ctx, userID)
+		if err != nil {
+			return err
+		}
+		userLevel = ul
+		return nil
+	})
+	return userLevel, err
+}
+
+func (s *service) UpdateUserLevelsLevel(ctx context.Context, arg postgres.UpdateUserLevelsLevelParams) error {
+	err := s.tx.Run(ctx, func(ctx context.Context) error {
+		err := s.repository.UpdateUserLevelsLevel(ctx, arg)
+		if err != nil {
+			return err
+		}
+		return err
+	})
+	return err
+}
