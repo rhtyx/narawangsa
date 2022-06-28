6cdb716bfd82cd26b758f37bbe625ca4e2fe63ed
Ebin Elbentor <ebinelbentor@Ebins-MacBook-Pro.local>
2022-06-28 15:51:32 +0700

    update interface.go in package categories - added ICategories

--- internal/domain/categories/interface.go
+++ internal/domain/categories/interface.go
@@ -1,1 +1,15 @@
 package categories
+
+import (
+	"context"
+
+	"github.com/rhtyx/narawangsa/internal/storage/postgres"
+)
+
+type ICategories interface {
+	CreateCategory(ctx context.Context, name string) error
+	DeleteCategory(ctx context.Context, name string) error
+	GetCategory(ctx context.Context, name string) (postgres.Category, error)
+	ListCategories(ctx context.Context, arg postgres.ListCategoriesParams) ([]postgres.Category, error)
+	UpdateCategory(ctx context.Context, arg postgres.UpdateCategoryParams) error
+}
