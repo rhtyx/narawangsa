d21b3d11142057aecb8e47c40036dcde0f2acc46
Ebin Elbentor <ebinelbentor@Ebins-MacBook-Pro.local>
2022-06-28 12:28:14 +0700

    update interface.go in package books - added interface IBooks

--- internal/domain/books/interface.go
+++ internal/domain/books/interface.go
@@ -1,1 +1,15 @@
 package books
+
+import (
+	"context"
+
+	"github.com/rhtyx/narawangsa/internal/storage/postgres"
+)
+
+type IBooks interface {
+	CreateBook(ctx context.Context, arg postgres.CreateBookParams) (int64, error)
+	DeleteBook(ctx context.Context, id int64) error
+	GetBook(ctx context.Context, title string) (postgres.Book, error)
+	ListBooks(ctx context.Context, limit int32) ([]postgres.Book, error)
+	UpdateBook(ctx context.Context, arg postgres.UpdateBookParams) error
+}
