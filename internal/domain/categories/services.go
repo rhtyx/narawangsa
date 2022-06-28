d44341b589868c317a820f19ce27040b52adcdb9
Ebin Elbentor <ebinelbentor@Ebins-MacBook-Pro.local>
2022-06-28 17:06:58 +0700

    update services.go in package categories - added NewCategoriesService functionality

--- internal/domain/categories/services.go
+++ internal/domain/categories/services.go
@@ -1,1 +1,70 @@
 package categories
+
+import (
+	"context"
+
+	"github.com/rhtyx/narawangsa/internal/storage/postgres"
+)
+
+type categoriesStorage interface {
+	CreateCategory(ctx context.Context, name string) error
+	DeleteCategory(ctx context.Context, name string) error
+	GetCategory(ctx context.Context, name string) (postgres.Category, error)
+	ListCategories(ctx context.Context, arg postgres.ListCategoriesParams) ([]postgres.Category, error)
+	UpdateCategory(ctx context.Context, arg postgres.UpdateCategoryParams) error
+}
+
+type service struct {
+	repository ICategories
+	tx         postgres.TxInContext
+}
+
+func NewCategoriesService(repository ICategories, tx postgres.TxInContext) categoriesStorage {
+	return &service{
+		repository: repository,
+		tx:         tx,
+	}
+}
+
+func (s *service) CreateCategory(ctx context.Context, name string) error {
+	err := s.tx.Run(ctx, func(ctx context.Context) error {
+		return s.repository.CreateCategory(ctx, name)
+	})
+	return err
+}
+func (s *service) DeleteCategory(ctx context.Context, name string) error {
+	err := s.tx.Run(ctx, func(ctx context.Context) error {
+		return s.repository.DeleteCategory(ctx, name)
+	})
+	return err
+}
+func (s *service) GetCategory(ctx context.Context, name string) (postgres.Category, error) {
+	var category postgres.Category
+	err := s.tx.Run(ctx, func(ctx context.Context) error {
+		c, err := s.repository.GetCategory(ctx, name)
+		if err != nil {
+			return err
+		}
+		category = c
+		return nil
+	})
+	return category, err
+}
+func (s *service) ListCategories(ctx context.Context, arg postgres.ListCategoriesParams) ([]postgres.Category, error) {
+	var categories []postgres.Category
+	err := s.tx.Run(ctx, func(ctx context.Context) error {
+		cs, err := s.repository.ListCategories(ctx, arg)
+		if err != nil {
+			return err
+		}
+		categories = cs
+		return nil
+	})
+	return categories, err
+}
+func (s *service) UpdateCategory(ctx context.Context, arg postgres.UpdateCategoryParams) error {
+	err := s.tx.Run(ctx, func(ctx context.Context) error {
+		return s.repository.UpdateCategory(ctx, arg)
+	})
+	return err
+}
