5654ebf4e87f6afc74c06c18fb8c853e7fcfbb53
Ebin Elbentor <ebinelbentor@Ebins-MacBook-Pro.local>
2022-07-01 00:58:01 +0700

    update server.go in package server - added function NewServer

--- http/server/server.go
+++ http/server/server.go
@@ -1,1 +1,19 @@
 package server
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/rhtyx/narawangsa/internal/storage"
+)
+
+type server struct {
+	store  storage.ExecTx
+	router *gin.Engine
+}
+
+func NewServer(store storage.ExecTx) *server {
+	server := &server{store: store}
+	router := gin.Default()
+
+	server.router = router
+	return server
+}
