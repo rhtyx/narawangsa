87be0de3906660119ce023f7b000ce1e9c683c84
Ebin Elbentor <ebinelbentor@Ebins-MacBook-Pro.local>
2022-07-01 11:36:25 +0700

    update response.go in package lib - added function Response

--- lib/response.go
+++ lib/response.go
@@ -1,1 +1,14 @@
 package lib
+
+func Response(status string, message string, obj interface{}) interface{} {
+	res := map[string]interface{}{
+		"status":  status,
+		"message": message,
+	}
+
+	if obj != nil {
+		res["data"] = obj
+	}
+
+	return res
+}
