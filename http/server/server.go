671f0e9fe24b93ddb7752208bf0dfefa9849eb9d
Ebin Elbentor <ebinelbentor@Ebins-MacBook-Pro.local>
2022-07-03 01:41:43 +0700

    update server.go in package server - added path to router

--- http/server/server.go
+++ http/server/server.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/rhtyx/narawangsa/http/handlers/base"
 	u "github.com/rhtyx/narawangsa/http/handlers/users"
+	"github.com/rhtyx/narawangsa/http/middleware"
 	"github.com/rhtyx/narawangsa/internal/domain/users"
 	"github.com/rhtyx/narawangsa/internal/storage"
 	"github.com/rhtyx/narawangsa/internal/storage/postgres"
@@ -28,11 +29,57 @@ func New(store *postgres.Queries, storetx *postgres.TxInContext, config lib.Conf
 	user := u.NewHandler(userService, token, config)
 
 	router.GET("/ping", base.Ping)
-	router.POST("/users/create", user.Create)
+
+	v1 := router.Group("/v1")
+	{
+		v1.POST("/signup", user.Create)
+		v1.POST("/login")
+		v1.GET("/logout")
+
+		users := v1.Group("/users").Use(middleware.AuthMiddleware(token))
+		{
+			users.GET("/", user.Get)
+			users.PUT("/")
+			users.PATCH("/update_password")
+			users.DELETE("/", user.Delete)
+		}
+
+		books := v1.Group("/books")
+		{
+			books.GET("/")
+			books.GET("/:book_id")
+			books.POST("/")
+			books.PUT("/:book_id")
+			books.DELETE("/:book_id")
+		}
+
+		categories := v1.Group("/categories")
+		{
+			categories.GET("/")
+			categories.POST("/")
+			categories.PUT("/:category_id")
+			categories.DELETE("/:category_id")
+		}
+
+		booklists := v1.Group("/booklists").Use(middleware.AuthMiddleware(token))
+		{
+			booklists.GET("/")
+			booklists.POST("/")
+			booklists.DELETE("/:book_id")
+		}
+
+		readConfirmations := v1.Group("/read_confirmations").Use(middleware.AuthMiddleware(token))
+		{
+			readConfirmations.GET("/")
+			readConfirmations.POST("/")
+		}
+
+		userLevels := v1.Group("/user_levels").Use(middleware.AuthMiddleware(token))
+		{
+			userLevels.PUT("/")
+		}
+	}
 	// TODO: change the uri to validation
-	router.GET("/users/:username", user.Get)
-	router.DELETE("/users/delete/:username", user.Delete)
-	router.POST("/users/update_password/:username")
 	server.router = router
 	return server
 }
