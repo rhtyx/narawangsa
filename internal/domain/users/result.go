bd32952a4c2aabbc028dabcfd2dc48cae25f4831
Ebin Elbentor <ebinelbentor@Ebins-MacBook-Pro.local>
2022-07-02 13:16:11 +0700

    update result.go in package users - added struct User

--- internal/domain/users/result.go
+++ internal/domain/users/result.go
@@ -1,1 +1,12 @@
 package users
+
+import "time"
+
+type User struct {
+	ID        int64     `json:"id"`
+	Name      string    `json:"name"`
+	Username  string    `json:"username"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
