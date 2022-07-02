d3e92ee68030bf76e4540a5b58f9428985060dfb
Ebin Elbentor <ebinelbentor@Ebins-MacBook-Pro.local>
2022-07-02 11:31:51 +0700

    update delete.go in package users - added func delete user

--- http/handlers/users/delete.go
+++ http/handlers/users/delete.go
@@ -1,1 +1,32 @@
 package users
+
+import (
+	"database/sql"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rhtyx/narawangsa/lib"
+)
+
+type deleteAccountRequest struct {
+	username string `uri:"username" binding:"required"`
+}
+
+func (h *handler) Delete(ctx *gin.Context) {
+	var req deleteAccountRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, lib.ErrorResponse(err))
+		return
+	}
+
+	err := h.service.DeleteUser(ctx, req.username)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, lib.ErrorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, lib.ErrorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, lib.Response("success", "user has been deleted", nil))
+}
