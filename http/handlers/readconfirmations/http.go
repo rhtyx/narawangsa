ce0d96087f65504b5a9165e104fc3dc78738f45c
Ebin Elbentor <ebinelbentor@Ebins-MacBook-Pro.local>
2022-07-03 17:32:04 +0700

    update http.go in package readconfirmations - added function NewHandler

--- http/handlers/readconfirmations/http.go
+++ http/handlers/readconfirmations/http.go
@@ -1,1 +1,13 @@
 package readconfirmations
+
+import "github.com/rhtyx/narawangsa/internal/domain/readconfirmations"
+
+type handler struct {
+	service readconfirmations.IReadConfirmations
+}
+
+func NewHandler(service readconfirmations.IReadConfirmations) *handler {
+	return &handler{
+		service: service,
+	}
+}
