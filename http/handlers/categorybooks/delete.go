51d5f744571692dd8a830d710a0f8c92600764f7
Ebin Elbentor <ebinelbentor@Ebins-MacBook-Pro.local>
2022-07-03 18:01:07 +0700

    update delete.go in package categorybooks handler - added function Delete

--- http/handlers/categorybooks/delete.go
+++ http/handlers/categorybooks/delete.go
@@ -1,1 +1,35 @@
 package categorybooks
+
+import (
+	"database/sql"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rhtyx/narawangsa/internal/storage/postgres"
+	"github.com/rhtyx/narawangsa/lib"
+)
+
+func (h *handler) Delete(ctx *gin.Context) {
+	var req categoryBooksRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, lib.ErrorResponse(err))
+		return
+	}
+
+	arg := postgres.DeleteBookCategoryParams{
+		BookID:     req.BookID,
+		CategoryID: req.CategoryID,
+	}
+
+	err := h.service.DeleteBookCategory(ctx, arg)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, lib.ErrorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, lib.ErrorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, lib.Response("success", "books category has been deleted", nil, nil))
+}
