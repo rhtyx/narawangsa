c356c2f8fb5a86a03a8075bfde42d21f601d5450
Ebin Elbentor <ebinelbentor@Ebins-MacBook-Pro.local>
2022-07-03 13:29:54 +0700

    update create.go in package categories - added function handler Create

--- http/handlers/categories/create.go
+++ http/handlers/categories/create.go
@@ -1,1 +1,36 @@
 package categories
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
+	"github.com/rhtyx/narawangsa/lib"
+)
+
+type createCategoryRequest struct {
+	Name string `json:"name" binding:"required"`
+}
+
+func (h *handler) Create(ctx *gin.Context) {
+	var req createCategoryRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, lib.ErrorResponse(err))
+		return
+	}
+
+	err := h.service.CreateCategory(ctx, req.Name)
+	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "unique_violation":
+				ctx.JSON(http.StatusForbidden, lib.ErrorResponse(err))
+				return
+			}
+		}
+		ctx.JSON(http.StatusInternalServerError, lib.ErrorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, lib.Response("success", "category has been created", nil, nil))
+}
