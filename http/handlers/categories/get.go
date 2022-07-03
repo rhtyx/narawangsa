2d93aa6cf81e28f1adb588d175e7ed532c2f73e7
Ebin Elbentor <ebinelbentor@Ebins-MacBook-Pro.local>
2022-07-03 14:40:56 +0700

    update get.go in package categories handler - added list if bindingJSON error

--- http/handlers/categories/get.go
+++ http/handlers/categories/get.go
@@ -1,17 +1,45 @@
 package categories
 
 import (
+	"database/sql"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/lib/pq"
+	"github.com/rhtyx/narawangsa/internal/storage/postgres"
 	"github.com/rhtyx/narawangsa/lib"
 )
 
 func (h *handler) Get(ctx *gin.Context) {
 	var req categoryRequest
 	if err := ctx.ShouldBindJSON(&req); err != nil {
-		ctx.JSON(http.StatusBadRequest, lib.ErrorResponse(err))
+		limit, err := strconv.Atoi(ctx.DefaultQuery("limit", "5"))
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, lib.ErrorResponse(err))
+			return
+		}
+		offset, err := strconv.Atoi(ctx.DefaultQuery("offset", "0"))
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, lib.ErrorResponse(err))
+			return
+		}
+
+		var req = postgres.ListCategoriesParams{
+			Limit:  int32(limit),
+			Offset: int32(offset),
+		}
+
+		response, err := h.service.ListCategories(ctx, req)
+		if err != nil {
+			if err == sql.ErrNoRows {
+				ctx.JSON(http.StatusNotFound, lib.ErrorResponse(err))
+				return
+			}
+			ctx.JSON(http.StatusInternalServerError, lib.ErrorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusOK, lib.Response("success", "categories has been returned", response, nil))
 		return
 	}
 
