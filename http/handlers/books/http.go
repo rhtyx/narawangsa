12f30cdf72f0086fda5c842748663dff1cd58636
Ebin Elbentor <ebinelbentor@Ebins-MacBook-Pro.local>
2022-07-03 15:59:34 +0700

    update http.go in package books handler - added func NewHandler

--- http/handlers/books/http.go
+++ http/handlers/books/http.go
@@ -1,1 +1,13 @@
 package books
+
+import "github.com/rhtyx/narawangsa/internal/domain/books"
+
+type handler struct {
+	service books.IBooks
+}
+
+func Newhandler(service books.IBooks) *handler {
+	return &handler{
+		service: service,
+	}
+}
