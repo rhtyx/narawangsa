59c75b5f989ed93ee598151ab6ee32711f8101f6
Ebin Elbentor <ebinelbentor@Ebins-MacBook-Pro.local>
2022-07-03 13:23:54 +0700

    update http.go in package categories handler - added func NewHandler

--- http/handlers/categories/http.go
+++ http/handlers/categories/http.go
@@ -1,1 +1,13 @@
 package categories
+
+import "github.com/rhtyx/narawangsa/internal/domain/categories"
+
+type handler struct {
+	service categories.ICategories
+}
+
+func NewHandler(service categories.ICategories) *handler {
+	return &handler{
+		service: service,
+	}
+}
