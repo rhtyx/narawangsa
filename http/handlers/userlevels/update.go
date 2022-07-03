5911fba14f9cc985f282c481fa3f09bd7b57ed13
Ebin Elbentor <ebinelbentor@Ebins-MacBook-Pro.local>
2022-07-03 12:36:01 +0700

    update update.go in package userlevels - added function Update

--- http/handlers/userlevels/update.go
+++ http/handlers/userlevels/update.go
@@ -1,1 +1,46 @@
 package userlevels
+
+import (
+	"database/sql"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rhtyx/narawangsa/http/middleware"
+	"github.com/rhtyx/narawangsa/internal/storage/postgres"
+	"github.com/rhtyx/narawangsa/internal/token"
+	"github.com/rhtyx/narawangsa/lib"
+)
+
+type updateUserLevelsRequest struct {
+	Level int32 `json:"level" binding:"required"`
+}
+
+func (h *handler) Update(ctx *gin.Context) {
+	authPayload := ctx.MustGet(middleware.AuthorizationPayloadKey).(*token.Payload)
+
+	var req updateUserLevelsRequest
+
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, lib.ErrorResponse(err))
+		return
+	}
+
+	arg := postgres.UpdateUserLevelsLevelParams{
+		UserID:    authPayload.UserId,
+		Level:     req.Level,
+		UpdatedAt: time.Now(),
+	}
+
+	err := h.service.UpdateUserLevelsLevel(ctx, arg)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, lib.ErrorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, lib.ErrorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, lib.Response("success", "user level has been updated", nil, nil))
+}
