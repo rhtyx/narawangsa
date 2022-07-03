7d9b558acf30e8ef3d020246d5640b6f22cf9444
Ebin Elbentor <ebinelbentor@Ebins-MacBook-Pro.local>
2022-07-03 11:42:12 +0700

    update http.go in package userlevels - added function NewHandler

--- http/handlers/userlevels/http.go
+++ http/handlers/userlevels/http.go
@@ -1,1 +1,18 @@
 package userlevels
+
+import (
+	"github.com/rhtyx/narawangsa/internal/domain/userlevels"
+	"github.com/rhtyx/narawangsa/internal/token"
+)
+
+type handler struct {
+	service userlevels.IUserLevels
+	token   token.Maker
+}
+
+func NewHandler(service userlevels.IUserLevels, token token.Maker) *handler {
+	return &handler{
+		service: service,
+		token:   token,
+	}
+}
