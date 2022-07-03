bec015f52b5ba735d1b7c1b92e4a9cb44b94ef36
Ebin Elbentor <ebinelbentor@Ebins-MacBook-Pro.local>
2022-07-03 18:50:06 +0700

    add create.go in package booklists handler - added function Create

--- http/handlers/booklists/create.go
+++ http/handlers/booklists/create.go
@@ -1,1 +1,50 @@
 package booklists
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
+	"github.com/rhtyx/narawangsa/internal/storage/postgres"
+	"github.com/rhtyx/narawangsa/lib"
+)
+
+type booklistRequest struct {
+	UserID    int64     `json:"user_id" binding:"required"`
+	BookID    int64     `json:"book_id" binding:"required"`
+	IsRead    bool      `json:"is_read" binding:"required"`
+	PagesRead int32     `json:"pages_read" binding:"required"`
+	EndDate   time.Time `json:"end_date" binding:"required" time_format:"2006-01-02"`
+}
+
+func (h *handler) Create(ctx *gin.Context) {
+	var req booklistRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusInternalServerError, lib.ErrorResponse(err))
+		return
+	}
+
+	arg := postgres.CreateBookListParams{
+		UserID:    req.UserID,
+		BookID:    req.BookID,
+		IsRead:    req.IsRead,
+		PagesRead: req.PagesRead,
+		EndDate:   req.EndDate,
+	}
+
+	err := h.service.CreateBookList(ctx, arg)
+	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "unique_violation":
+				ctx.JSON(http.StatusForbidden, lib.ErrorResponse(err))
+				return
+			}
+		}
+		ctx.JSON(http.StatusInternalServerError, lib.ErrorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, lib.Response("success", "booklist has been created", nil, nil))
+}
