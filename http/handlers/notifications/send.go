79c4675c6543682c4a399e117c8692d99a07d643
Ebin Elbentor <ebinelbentor@Ebins-MacBook-Pro.local>
2022-07-06 14:26:20 +0700

    update send.go in package notifications handler - added func Send handler

--- http/handlers/notifications/send.go
+++ http/handlers/notifications/send.go
@@ -1,1 +1,32 @@
 package notifications
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rhtyx/narawangsa/lib"
+)
+
+type sendNotificationRequest struct {
+	Name  string `json:"name" binding:"required"`
+	Email string `json:"email" binding:"required,email"`
+	Quote string `json:"quote" binding:"required"`
+}
+
+func (h *handler) Send(ctx *gin.Context) {
+	var req sendNotificationRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, lib.ErrorResponse(err))
+		return
+	}
+
+	ctx.Set("name", req.Name)
+	ctx.Set("email", req.Email)
+	ctx.Set("quote", req.Quote)
+
+	err := h.service.SendNotifications(ctx)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, lib.ErrorResponse(err))
+		return
+	}
+}
