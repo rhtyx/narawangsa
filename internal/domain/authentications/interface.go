c062c30fb563af1a37a28810e964ed92577df643
Ebin Elbentor <ebinelbentor@Ebins-MacBook-Pro.local>
2022-07-06 22:49:46 +0700

    update interface.go in package authentications - added interface IAuthentications

--- internal/domain/authentications/interface.go
+++ internal/domain/authentications/interface.go
@@ -1,1 +1,9 @@
 package authentications
+
+import "context"
+
+type IAuthentications interface {
+	CreateRefreshToken(ctx context.Context, refreshToken string) error
+	DeleteRefreshToken(ctx context.Context, refreshToken string) error
+	GetRefreshToken(ctx context.Context, refreshToken string) (string, error)
+}
