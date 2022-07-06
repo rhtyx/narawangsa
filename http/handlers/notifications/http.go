1aed637ed409ed13d0e5133cb68f95de5471fef6
Ebin Elbentor <ebinelbentor@Ebins-MacBook-Pro.local>
2022-07-06 14:14:32 +0700

    update http.go in package notifications handler - added func NewService

--- http/handlers/notifications/http.go
+++ http/handlers/notifications/http.go
@@ -1,1 +1,13 @@
 package notifications
+
+import "github.com/rhtyx/narawangsa/internal/domain/notifications"
+
+type handler struct {
+	service notifications.INotifications
+}
+
+func NewService(service notifications.INotifications) *handler {
+	return &handler{
+		service: service,
+	}
+}
